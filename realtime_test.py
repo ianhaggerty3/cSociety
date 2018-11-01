2d7279af09cca0c9736eba9f498b04f33f2831e4
Ian Haggerty <ianhaggerty3@gmail.com>
2018-11-01 12:43:00 -0400

    Updated documentation a bit

--- realtime_test.py
+++ realtime_test.py
@@ -12,6 +12,7 @@ fid.close()
 img_rows, img_cols = 640, 480
 num_category = 5
 categories = {0: 'closed_fist', 1: 'one_finger', 2: 'open_hand', 3: 'thumbs_up', 4: 'two_fingers'}
+
 # 0 front camera
 # 1 back camera
 camera = 0
@@ -30,19 +31,16 @@ def realtime_test(secsBetweenSamples: float):
             X_test = cv2.resize(img, (640, 480))
 
             # Run the image through the neural network
-            '''if kbackend.image_data_format() == 'channels_first':
-                X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-            else:
-                print('I use the second part of the if statement')
-                X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)'''
-            X_test = X_test.reshape(1, img_rows, img_cols, 1)
+            X_test = X_test.reshape(1, img_rows, img_cols, 1) #One BW image is 1 x 640 x 480 x 1
             prediction = model.predict(X_test)
-            # display image
+
+            # Display image with the correct prediction in text format
             index = numpy.argmax(prediction)
             cv2.putText(frame, categories[index], (30, 30), font, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
             cv2.imshow('current image', frame)
             cv2.waitKey(1)
             time.sleep(secsBetweenSamples)
+
     except KeyboardInterrupt:
         # Note: Pycharm KeyboardInterrupt is ctrl + f2, normally it is ctrl + c
         print('')
